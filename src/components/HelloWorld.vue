dce66f0726bc11c2290681f0222d827502348dbf
yosi <yosihofman21@gmail.com>
2024-09-17 17:37:32 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,236 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+      <v-layout align-center justify-center v-if="!isLoading">
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>Login form</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  name="email"
+                  label="email"
+                  type="email"
+                  v-model="login.email"
+                ></v-text-field>
+                <v-text-field
+                  :type="show1 ? 'text' : 'password'"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  @click:append="show1 = !show1"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  v-model="login.password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-subtitle >
+            
+              <router-link 
+     
+         class=""
+            style="text-decoration: none"
+       to="" @click="sendEmail">
+
+       <p @click="sendEmail">שכחתי סיסמה</p>
+       </router-link>
+            </v-card-subtitle>
+          
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn color="success" @click="userLogin"> התחבר</v-btn>
+              <v-spacer></v-spacer>
+              <v-row>
+                <v-dialog v-model="dialog" persistent max-width="290">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                      הירשם
+                    </v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">User Profile</span>
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container>
+                        <v-form ref="form" lazy-validation>
+                          <v-row>
+                            <v-col cols="12">
+                              <v-text-field
+                                label="user name"
+                                v-model="user.usernamea"
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-text-field
+                                :rules="emailRules"
+                                label="Email*"
+                                v-model="user.emaila"
+                                required
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-text-field
+                                :type="show1 ? 'text' : 'password'"
+                                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                @click:append="show1 = !show1"
+                                :rules="[rules.required, rules.min]"
+                                label="Password*"
+                                v-model="user.passworda"
+                                required
+                              ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-text-field
+                                :type="show2 ? 'text' : 'password'"
+                                :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+                                :rules="[rules.required, rules.min]"
+                                @click:append="show2 = !show2"
+                                label="Confirm Password*"
+                                v-model="confirmPassword"
+                                required
+                              ></v-text-field>
+                            </v-col>
+                          </v-row>
+                        </v-form>
+                      </v-container>
+                      <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="blue darken-1" text @click="dialog = false">
+                        Close
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="
+                          register();
+                          dialog = false;
+                        "
+                      >
+                        Save
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </v-card-actions>
+          </v-card>
+       
+       
+
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="isLoading" align-center justify-center>
+            <v-progress-circular
+ 
+   indeterminate
+   color="primary"
+></v-progress-circular>
+
+      </v-layout>
+  </v-container>
 </template>
 
 <script>
+// import firebase from "firebase";
+// import { db } from '../main.js';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      isLoading: false,
+      show1: false,
+      show2: false,
+      emailRules: [
+        (v) => !!v || "חובה",
+        (v) => /.+@.+\..+/.test(v) || " יש להזין כתובת אימייל חוקית ",
+      ],
+      rules: {
+        required: (value) => !!value || "חובה.",
+        min: (v) => v.length >= 4 || "מינימום 4 תווים",
+      },
+      dialog: false,
+
+      login:{
+     
+        email: "",
+        password: "",
+
+      },
+      user: {   
+        usernamea: "",
+      emaila: "",
+      passworda: "",
+      },
+
+      confirmPassword: "",
+   
+      id: null,
+    };
+  },
+
+  methods: {
+        async someMethod() {
+      // Using Firestore
+      const docRef = doc(this.$db, "collection", "document");
+      const docSnap = await getDoc(docRef);
+
+      // Using Auth
+      const user = this.$auth.currentUser;
+    },
+
+    sendEmail() {
+        firebase
+        .auth()
+        .sendPasswordResetEmail(this.login.email)
+        .then(() => {
+            alert('Check your registered email to reset the password!')
+            this.login = {   
+              email: ''
+            }
+        }).catch((error) => {
+          alert(error)
+        })
+    },
+        userLogin() {
+        firebase
+        .auth()
+        .signInWithEmailAndPassword(this.login.email, this.login.password)
+          .then(() => {
+            console.log("logged in");
+ 
+
+        
+        
+            //           let clientData = {
+            //   // id: data.id,
+            //   name: this.login.email,
+            //   // admin: data.admin,
+
+            //   // token: data.newToken
+            // };
+            // localStorage.setItem("user", JSON.stringify(clientData));
+
+            // this.checkLocal(JSON.parse(localStorage.user));
+            // // console.log(this.clientInfo)
+            this.$router.push('/fixtures')
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+  }
+  }
+
+
+
+
+
+
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,40 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-createApp(App).use(store).use(router).mount('#app')
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPoeHBjqNEYksEnN0rlkgdbtxj-_0X0-A",
+  authDomain: "businesses-239b9.firebaseapp.com",
+  projectId: "businesses-239b9",
+  storageBucket: "businesses-239b9.appspot.com",
+  messagingSenderId: "964516205695",
+  appId: "1:964516205695:web:4e446876b4557676561b4a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+// Create Vue app
+const app = createApp(App);
+
+// Make Firebase services available in all components
+app.config.globalProperties.$auth = auth;
+app.config.globalProperties.$db = db;
+
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
